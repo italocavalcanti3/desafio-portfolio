e689c57825240567463f347b057666e5cc0c5ba1
Italo Cavalcanti <italobc93@gmail.com>
2023-03-01 22:21:31 -0300

    recebe dados da api do github

--- scripts.js
+++ scripts.js
@@ -1,17 +1,23 @@
-const reposInfo = [];
+let userRepos = []
 
-async function getUserData(username) {
-  const userInfo = await fetch(`https://api.github.com/users/${username}/repos`)
+async function fetchGithubRepositories(username) {
+  await fetch(`https://api.github.com/users/italocavalcanti3/repos`)
   .then(response => response.json())
-  .then(data => { return data });
-  
-  reposInfo.push({
-    name: userInfo.name,
-    url: userInfo.html_url,
-    description: userInfo.description,
-  })
+  .then(repos => {
+    repos.forEach(repo => {
+      userRepos.push({
+        id: repo.id,
+        name: repo.name,
+        description: repo.description,
+        url: repo.html_url,
+        language: repo.language,
+        stars: repo.stargazers_count,
+        forks: repo.forks_count,
+      })
+    });
+  });
 
-  console.log(reposInfo)
+  console.log(userRepos);
 }
 
-getUserData('italocavalcanti3');
+fetchGithubRepositories('italocavalcanti3');
